1bb7cb46abee5bb3b51f48b0990b4653571dba9d
Javier Jimenez <javierrcc522@gmail.com>
2018-04-05 16:38:52 -0700

    new sh script to copy paste files

--- scp_car_data.sh
+++ scp_car_data.sh
@@ -1,5 +1,6 @@
 IP=$1
 SESSION=$2
 # Example: sh scp_car_data.sh 192.168.1.82 152
-scp pi@${IP}:/home/pi/Desktop/session/${SESSION}/* /Users/javi/Desktop/${SESSION}/
+mkdir -p /Users/javi/Desktop/${SESSION}/
+scp pi@${IP}:/home/pi/Desktop/Autonomous-RC-car/*session.txt /Users/javi/Desktop/${SESSION}/
 echo "Finished"
